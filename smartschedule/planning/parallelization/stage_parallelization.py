f895c6bbf4518720cf9c483878117fbf86f0ca1d
Piotr Wysocki <hello@piotrwysocki.com>
2024-05-17 22:43:40 +0200

    Add: Functional workflow if you really like it 🤷

--- smartschedule/planning/parallelization/stage_parallelization.py
+++ smartschedule/planning/parallelization/stage_parallelization.py
@@ -1,15 +1,12 @@
-from smartschedule.planning.parallelization.parallel_stages_list import (
-    ParallelStagesList,
-)
 from smartschedule.planning.parallelization.sorted_nodes_to_parallelized_stages import (
     sorted_nodes_to_parallelized_stages,
 )
-from smartschedule.planning.parallelization.stage import Stage
 from smartschedule.planning.parallelization.stages_to_nodes import stages_to_nodes
 from smartschedule.sorter import graph_topological_sort
+from smartschedule.utils.pipe import pipe
 
-
-def stage_parallelization_of(stages: set[Stage]) -> ParallelStagesList:
-    nodes = stages_to_nodes(stages)
-    sorted_nodes = graph_topological_sort(nodes)
-    return sorted_nodes_to_parallelized_stages(sorted_nodes)
+stage_parallelization_of = pipe(
+    stages_to_nodes,
+    graph_topological_sort,
+    sorted_nodes_to_parallelized_stages,
+)
